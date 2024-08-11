7d44e5eadc4b2a06ec2e92e8359ffbe6918fda35
canario2545 <160178751+canario2545@users.noreply.github.com>
2024-08-10 22:30:38 -0400

    Add files via upload

---
+++ LibraryLogica/LibroService.cs
@@ -0,0 +1,44 @@
+﻿using LibraryDate;
+using LibraryEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLogica
+{
+    public class LibroService
+    {
+        private readonly LibroDAL libroDAL;
+        public LibroService()
+        {
+            libroDAL = new LibroDAL();
+        }
+
+        public List<Libro> ObtenerLibros()
+        {
+            return libroDAL.ObtenerLibros();
+        }
+
+        public void AgregarLibro(Libro libro)
+        {
+            libroDAL.AgregarLibro(libro);
+        }
+
+        public void ActualizarLibro(Libro libro)
+        {
+            libroDAL.ActualizarLibro(libro);
+        }
+
+        public void EliminarLibro(int id)
+        {
+            libroDAL.EliminarLibro(id);
+        }
+
+        public List<Libro> BuscarLibros(string query)
+        {
+            return libroDAL.BuscarLibros(query);
+        }
+    }
+}
