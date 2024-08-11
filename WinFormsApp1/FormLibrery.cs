cdceafad08656136aa9674d27659c56d23f8d4c2
canario2545 <160178751+canario2545@users.noreply.github.com>
2024-08-10 22:36:57 -0400

    Add files via upload

---
+++ WinFormsApp1/FormLibrery.cs
@@ -0,0 +1,41 @@
+﻿using LibraryLogica;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class FormLibrery : Form
+    {
+        private LibroService libroService;
+
+        public FormLibrery()
+        {
+            InitializeComponent();
+            libroService = new LibroService();
+            CargarLibros();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CargarLibros()
+        {
+            var libros = libroService.ObtenerLibros();
+            dataGridView1.DataSource = libros;
+        }
+
+        private void FormLibrery_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
